c9f4c316314b4fb60ba63670d019e1663af53883
Tatyana Soloveva <tanuysya@mail.ru>
2024-01-03 13:42:23 +0300

    Написан метод по проверке количества символов в каждом элементе массива

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿string[] array = new string[5] {"1678", "88", "Tanya", "yes", "no", "go", "hello"};
 string[] newarray = new string[array.Length];
 void SecondArrayWithIF(string[] array, string[] newarray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        newarray[count] = array[i];
+        count++;
+        }
+    }
+}
