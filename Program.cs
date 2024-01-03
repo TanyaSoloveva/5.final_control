b70c99c09002c7b7771127f7db01684d3b6311e1
Tatyana Soloveva <tanuysya@mail.ru>
2024-01-03 13:33:43 +0300

    Задан массив

--- Program.cs
+++ Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿string[] array = new string[5] {"1678", "88", "Tanya", "yes", "no", "go", "hello"};
+string[] newarray = new string[array.Length];
+void SecondArrayWithIF(string[] array, string[] newarray)
